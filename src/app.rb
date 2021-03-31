bedc7f88ebaa519d8ea9a5844649a77655173c69
Noe Hsu <noe13hsu@gmail.com>
2021-03-31 21:37:15 +1000

    added head_data.rb and app.rb, created hash keys to store parts data and linked to app.rb

---
+++ src/app.rb
@@ -0,0 +1,5 @@
+require_relative "data.rb"
+
+a = $data[0]
+
+puts a
---
+++ src/head_data.rb
@@ -0,0 +1,19 @@
+$head_data = [
+    {
+     name: , 
+     type: , 
+     armor: ,
+     melee_atk: , 
+     shot_atk: , 
+     phys_res: , 
+     beam_res: , 
+     melee_def: , 
+     shot_def: , 
+     word_tag: [], 
+     ex_skill: {name: , type: , pierce: , power: , cooldown: }, 
+     trait: {type: , description: }
+    },
+    {
+
+    },
+]
