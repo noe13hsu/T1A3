385f6ba3088a2a924c4bc5c45921f2a23cd222d9
Noe Hsu <noe13hsu@gmail.com>
2021-04-05 21:46:56 +1000

    created a method to calculate total stats

--- src/app.rb
+++ src/app.rb
@@ -53,6 +53,22 @@ def request_password(message)
     return gets.chomp.downcase
 end
 
+def to_update_build?(user_choice_category, user_choice_part, this_user)
+    prompt = TTY::Prompt.new
+    answer = prompt.select("Would you like to update your build?") do |menu|
+        menu.choice "Yes"
+        menu.choice "No"
+    end
+    case answer
+    when "Yes"
+        this_user[:"#{user_choice_category}"] = user_choice_part
+        puts "Build updated"
+        # puts this_user, user_choice_category, user_choice_part
+    when "No"
+        puts "Build not updated"
+    end
+end
+
 def category_menu
     prompt = TTY::Prompt.new
     prompt.select("Please select a category") do |menu|
@@ -117,27 +133,52 @@ def title_menu
     end
 end
 
+def sum_stat(this_user)
+    categories = ["head", "body", "arm", "leg", "back", "weapon_melee", "weapon_ranged", "shield", "pilot"]
+    user_stats = {
+        armor: 0,
+        melee_atk: 0,
+        shot_atk: 0,
+        melee_def: 0,
+        shot_def: 0,
+        beam_res: 0,
+        phys_res: 0
+    }
+    i = 0
+    while i < categories.length do
+        CSV.foreach("#{categories[i]}.csv", :quote_char => "|", headers: true, header_converters: :symbol) do |row|
+            if row[:name] == this_user[:"#{categories[i]}"]
+                user_stats[:armor] += row[:armor].to_i
+                user_stats[:melee_atk] += row[:melee_atk].to_i
+                user_stats[:shot_atk] += row[:shot_atk].to_i
+                user_stats[:melee_def] += row[:melee_def].to_i
+                user_stats[:shot_def] += row[:shot_def].to_i
+                user_stats[:beam_res] += row[:beam_res].to_i
+                user_stats[:phys_res] += row[:phys_res].to_i
+            end
+        end
+        i += 1
+    end
+    return user_stats
+end
+
 def create_user_data_table(this_user)
-    TTY::Table.new(
+    user_stats = sum_stat(this_user)
+    current_build = TTY::Table.new(
         [   "Part",             "Name",                         "  ",   "Type",       "S"],
         [
-            ["Head",            this_user[:head],            "  ",   "Armor",      this_user[:head]], 
-            ["Body",            this_user[:body],            "  ",   "Melee ATK",  this_user[:body]], 
-            ["Arm",             this_user[:arm],             "  ",   "Shot ATK",   this_user[:arm]], 
-            ["Leg",             this_user[:leg],             "  ",   "Melee DEF",  this_user[:leg]], 
-            ["Back",            this_user[:back],            "  ",   "Shot DEF",   this_user[:back]], 
-            ["Melee Weapon",    this_user[:weapon_melee],    "  ",   "Beam RES",   this_user[:weapon_melee]], 
-            ["Ranged Weapon",   this_user[:weapon_ranged],   "  ",   "Phys RES",   this_user[:weapon_ranged]], 
+            ["Head",            this_user[:head],            "  ",   "Armor",      user_stats[:armor]], 
+            ["Body",            this_user[:body],            "  ",   "Melee ATK",  user_stats[:melee_atk]], 
+            ["Arm",             this_user[:arm],             "  ",   "Shot ATK",   user_stats[:shot_atk]], 
+            ["Leg",             this_user[:leg],             "  ",   "Melee DEF",  user_stats[:melee_def]], 
+            ["Back",            this_user[:back],            "  ",   "Shot DEF",   user_stats[:shot_def]], 
+            ["Melee Weapon",    this_user[:weapon_melee],    "  ",   "Beam RES",   user_stats[:beam_res]], 
+            ["Ranged Weapon",   this_user[:weapon_ranged],   "  ",   "Phys RES",   user_stats[:phys_res]], 
             ["Shield",          this_user[:shield],          "  ",   nil,          nil], 
             ["Pilot",           this_user[:pilot],           "  ",   nil,          nil]
         ]
-    )  
-end
-
-def search_and_display_parts
-    user_choice_category = category_menu.downcase
-    user_choice_part = request_part_name("Please enter a Gundam name: ")
-    create_parts_data_table(user_choice_category, user_choice_part)
+    )
+    puts current_build.render(:unicode, alignments: [:left, :center])  
 end
 
 def load_all_users
@@ -158,6 +199,13 @@ def load_parts(category)
     return all_parts
 end
 
+def search_and_display_parts
+    user_choice_category = category_menu.downcase
+    user_choice_part = request_part_name("Please enter a Gundam name: ")
+    is_table_created = create_parts_data_table(user_choice_category, user_choice_part)
+    return user_choice_part, user_choice_category, is_table_created
+end
+
 def sort_and_display_parts
     prompt = TTY::Prompt.new
     user_choice_category = category_menu.downcase
@@ -170,6 +218,7 @@ def sort_and_display_parts
         end
     end
     create_parts_data_table(user_choice_category, user_choice_part)
+    return user_choice_part, user_choice_category
 end
 
 def create_parts_data_table(user_choice_category, user_choice_part)
@@ -207,6 +256,7 @@ def create_parts_data_table(user_choice_category, user_choice_part)
         end
     end
     puts "Invalid name".colorize(:red)
+    return false
 end
 
 def reset_build(users, this_user)
@@ -252,8 +302,8 @@ when "Sign up"
         user_choice_feature = feature_menu
         case user_choice_feature
         when "Review my current build"
-            current_build = create_user_data_table(this_user)
-            puts current_build.render(:unicode, alignments: [:left, :center])
+            create_user_data_table(this_user)
+            
         when "Start a new build"
             puts "a"
         when "Search for parts by name"
@@ -281,15 +331,20 @@ when "Log in"
                 user_choice_feature = feature_menu
                 case user_choice_feature
                 when "Review my current build"
-                    current_build = create_user_data_table(this_user)
-                    puts current_build.render(:unicode, alignments: [:left, :center])
+                    create_user_data_table(this_user)
                 when "Start a new build"
                     reset_build(users, this_user)
                     write_to_csv(users)
                 when "Search for parts by name"
-                    search_and_display_parts
+                    user_choice_part, user_choice_category, is_table_created = search_and_display_parts
+                    if is_table_created
+                        to_update_build?(user_choice_category, user_choice_part, this_user)
+                    end
+                    write_to_csv(users)
                 when "Filter and sort parts"
-                    sort_and_display_parts
+                    user_choice_part, user_choice_category = sort_and_display_parts
+                    to_update_build?(user_choice_category, user_choice_part, this_user)
+                    write_to_csv(users)
                 when "Get a build recommendation"
                     puts "d"
                 when "Log out"
